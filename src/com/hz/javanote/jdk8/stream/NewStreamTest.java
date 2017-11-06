1dde4cd9076ebacad3ed8ac6dc2d214a2aa08686
zheh <zheh@vmware.com>
2017-11-06 20:17:24 +0800

    tes

--- src/com/hz/javanote/jdk8/stream/NewStreamTest.java
+++ src/com/hz/javanote/jdk8/stream/NewStreamTest.java
@@ -1,9 +1,9 @@
 package com.hz.javanote.jdk8.stream;
 
+import static java.util.stream.Collectors.groupingBy;
 import static java.util.stream.Collectors.toList;
 import static java.util.stream.Collectors.toMap;
-import static java.util.stream.Collectors.groupingBy;
-import java.util.Arrays;
+
 import java.util.List;
 import java.util.Map;
 import java.util.function.Function;
@@ -11,7 +11,7 @@ import java.util.function.Function;
 public class NewStreamTest {
 
 	public static void main(String[] args) {
-		List<Dish> menu=getMenu();
+		List<Dish> menu=Dish.getMenu();
 		
 		//printMap(getAllDishNameDishMap(menu));
 		printMap(getAllDishNameDishListMap(menu));
@@ -45,50 +45,5 @@ public class NewStreamTest {
 		return result;
 	}
 	
-	public static List<Dish> getMenu() {
-		List<Dish> menu = Arrays.asList(
-				new Dish("pork", false, 800, Dish.Type.MEAT),
-				new Dish("beef", false, 700, Dish.Type.MEAT),
-				new Dish("chicken", false, 400, Dish.Type.MEAT),
-				new Dish("french fries", true, 530, Dish.Type.OTHER),
-				new Dish("rice", true, 350, Dish.Type.OTHER),
-				new Dish("season fruit", true, 120, Dish.Type.OTHER),
-				new Dish("pizza", true, 550, Dish.Type.OTHER),
-				new Dish("prawns", false, 300, Dish.Type.FISH),
-				new Dish("salmon", false, 450, Dish.Type.FISH) );		
-		return menu;
-	}
 	
-	
-	public static class Dish {
-		private final String name;
-		private final boolean vegetarian;
-		private final int calories;
-		private final Type type;
-		
-		public Dish(String name, boolean vegetarian, int calories, Type type) {
-			this.name = name;
-			this.vegetarian = vegetarian;
-			this.calories = calories;
-			this.type = type;
-		}
-		public String getName() {
-			return name;
-		}
-		@Override
-		public String toString() {
-			return "Dish [name=" + name + ", vegetarian=" + vegetarian + ", calories=" + calories + ", type=" + type
-					+ "]";
-		}
-		public boolean isVegetarian() {
-			return vegetarian;
-		}
-		public int getCalories() {
-			return calories;
-		}
-		public Type getType() {
-			return type;
-		}
-		public enum Type { MEAT, FISH, OTHER }
-	}
 }
